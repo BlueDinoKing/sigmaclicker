a27d09e0d9d27bd28af2806d0f97e888f8d9a414
BlueDinoKing <80439860+BlueDinoKing@users.noreply.github.com>
2024-08-04 11:32:22 -0700

    successfully rizzed up baddies

--- scenes/clicker.gd
+++ scenes/clicker.gd
@@ -1,4 +1,11 @@
+class_name clicker
 extends Control
+
+## exports label so you can use it for stuff
+@export var label : Label 
+
+var rizz : int = 0
+
 
 
 # Called when the node enters the scene tree for the first time.
@@ -9,3 +16,17 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+## when rizz up baddies is pressed  rizz up baddies
+func _on_button_pressed() -> void:
+	rizzupbaddies()
+
+## rizz up bro
+func rizzupbaddies() -> void:
+	var addedRizz = 1
+	rizz += addedRizz
+	update_rizz()
+
+## updates the rizz label
+func update_rizz() -> void:
+	label.text = "Rizz : %s" %rizz
