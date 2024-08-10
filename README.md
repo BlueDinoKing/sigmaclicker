c12cb12e17d1839181d064f17d6065ae0614e06e
BlueDinoKing <80439860+BlueDinoKing@users.noreply.github.com>
2024-08-10 12:04:09 -0700

    idk

--- README.md
+++ README.md
@@ -3,6 +3,3 @@
 Latest Android Release: https://github.com/BlueDinoKing/sigmaclicker/blob/notwrizz/docs/index.apk
 
 Web Release: https://sigmaclicker.netlify.app/
-
-test
-Version: 2
--- scenes/rizz_up_baddies.gd
+++ scenes/rizz_up_baddies.gd
@@ -13,20 +13,25 @@ func _ready() -> void:
 
 func random_sign() -> int:
 	return 1 if randi() % 2 == 0 else -1
-
+#test
 func _on_pressed() -> void:
 	var rizzLabel: Label = Label.new()
-	rizzLabel.global_position = Vector2(264, 126)
+	# Calculate the center position of the TextureButton
+	var button_center = size / 2
+	rizzLabel.global_position = button_center
+	rizzLabel.global_position.x += 20
 	if GameInstance.data.upgrades[0] == 0:
-		rizzLabel.text = Game.format_number((floor(GameInstance.data.multiplier*GameInstance.data.tempMulti*(1 + GameInstance.data.goldChains))))
+		rizzLabel.text = Game.format_number(floor(GameInstance.data.multiplier * GameInstance.data.tempMulti * (1 + GameInstance.data.goldChains)))
 	else:
-		rizzLabel.text = Game.format_number((floor(GameInstance.data.multiplier*GameInstance.data.tempMulti*(1 + GameInstance.data.goldChains+(GameInstance.data.aura*(GameInstance.data.goldChains*.025*GameInstance.data.upgrades[0]))))))
+		rizzLabel.text = Game.format_number(floor(GameInstance.data.multiplier * GameInstance.data.tempMulti * (1 + GameInstance.data.goldChains + (GameInstance.data.aura * (GameInstance.data.goldChains * 0.025 * GameInstance.data.upgrades[0])))))
+
 	call_deferred("add_child", rizzLabel)
 	rizzLabel.pivot_offset = Vector2(rizzLabel.size / 2)
+	
 	var tween = get_tree().create_tween()
 	tween.set_parallel(true)
-	tween.tween_property(rizzLabel, "position:y", rizzLabel.position.y + randi_range(100, 200) * random_sign(), 1).set_ease(Tween.EASE_OUT)
-	tween.tween_property(rizzLabel, "position:x", rizzLabel.position.x + randi_range(100, 200) * random_sign(), 1).set_ease(Tween.EASE_OUT)
+	tween.tween_property(rizzLabel, "position:y", rizzLabel.position.y + randi_range(200, 300) * random_sign(), 1).set_ease(Tween.EASE_OUT)
+	tween.tween_property(rizzLabel, "position:x", rizzLabel.position.x + randi_range(200, 300) * random_sign(), 1).set_ease(Tween.EASE_OUT)
 	tween.tween_property(rizzLabel, "scale", Vector2.ZERO, 0.25).set_ease(Tween.EASE_IN).set_delay(0.5)
 	await tween.finished
 	rizzLabel.queue_free()
