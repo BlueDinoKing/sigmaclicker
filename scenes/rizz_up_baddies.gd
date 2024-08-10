126042914d5d154c4539b874b93f535122d00e4f
BlueDinoKing <80439860+BlueDinoKing@users.noreply.github.com>
2024-08-10 12:18:24 -0700

    testing something again maybe it owrks

--- scenes/rizz_up_baddies.gd
+++ scenes/rizz_up_baddies.gd
@@ -13,7 +13,7 @@ func _ready() -> void:
 
 func random_sign() -> int:
 	return 1 if randi() % 2 == 0 else -1
-#test
+#test2
 func _on_pressed() -> void:
 	var rizzLabel: Label = Label.new()
 	# Calculate the center position of the TextureButton
