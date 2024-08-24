6aea797d095e39f310a4754869f4aaaac42265d6
BlueDinoKing <80439860+BlueDinoKing@users.noreply.github.com>
2024-08-24 16:41:49 -0700

    release wohoo

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1723784747|2240453542';
+const CACHE_VERSION = '1724542731|220850812';
 /** @type {string} */
 const CACHE_PREFIX = 'sigmaclicker-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
